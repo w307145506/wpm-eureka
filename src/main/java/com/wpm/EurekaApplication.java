86dc84436b563dfcf89faae8d3646f35afb69c76
Fubao-A70 <Fubao-A70@Fuabo-A71>
2018-10-25 10:59:40 +0800

    初始导入

---
+++ src/main/java/com/wpm/EurekaApplication.java
@@ -0,0 +1,25 @@
+package com.wpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaClient 开启eureka服务
+ * @author Fubao-A70
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekaApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
